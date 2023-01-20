6a6abec9463974af9269be495795f90f9627283b
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-01-20 15:26:21 +0530

    Create critical_info.py

---
+++ critical_info.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+from googlesearch import search
+import requests
+from bs4 import BeautifulSoup
+import json
+
+st.title("Critical information nearby me")
+query = st.text_input("Enter your query")
+api_key="AIzaSyDEhUWGKuYW8G3JR3CpStnveTqu1gXrBD4"
+cx="f23358939906b4e32"
+
+if st.button("Search"):
+        def search(query,api_key,cx):
+          query = query.replace(" ", "+")
+          URL = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={cx}&q={query}"
+          r = requests.get(url=URL)
+          data = r.json()
+          return data
+        results = search(query,api_key,cx)
+        
+        for i in range(10):
+          text = results['items'][i]['snippet']
+          link = results['items'][i]['link']
+          title = results['items'][i]['title']
+          df = pd.DataFrame({'Description': [text], 'link': [link],'title':[title]})
+          st.table(df)
+
+         
+         
+          
+          
+     
+
+           
+         
+        
+
+        
+          
+
+
+ 
