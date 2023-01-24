f4ff318708178e8d6dde99c0a326976a6a7c1d1f
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-01-24 11:52:25 +0530

    Update NEWS_SCRAPPER.py

--- NEWS_SCRAPPER.py
+++ NEWS_SCRAPPER.py
@@ -30,7 +30,7 @@ def top_headlines():
                                        
    
         
-    if news["status"] == "ok":
+    
         st.write("Results Found : ", news["totalResults"])
         df = pd.DataFrame(news['articles'])
         st.dataframe(df)
